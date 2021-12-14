dca73fcbdca5b99cc62391763f076078a270bb15
Kiersten Gamby <kierstengamby@gmail.com>
2021-12-14 11:49:37 -0500

    incomes updated

--- src/components/Income/IncomeTable.jsx
+++ src/components/Income/IncomeTable.jsx
@@ -4,25 +4,25 @@ import { Table, Button } from 'reactstrap';
 const IncomeTable = (props) => {
 
     const deleteIncome = (incomeBudget) => {
-        fetch(`https://localhost:6000/income/${incomeBudget.id}`, {
+        fetch(`https://localhost:3001/income/${incomeBudget.id}`, {
             method: 'DELETE',
             headers: new Headers({
                 'Content-Type': 'application/json',
                 'Authorization': props.token
             })
         })
-        .then(() => props.fetchIncome())
+        .then(() => props.fetchIncomes())
     }
 
     const incomeMapper = () => {
-        return props.incomeBudget.map((incomeBudget, index) => {
+        return props.incomeBudgets.map((incomeBudget, index) => {
             return(
                 <tr key={index}>
                     <th scope="row">{incomeBudget.id}</th>
-                    <td>{incomeBudget.Paychecks}</td>
-                    <td>{incomeBudget.Investments}</td>
-                    <td>{incomeBudget.Reimbursements}</td>
-                    <td>{incomeBudget.Misc}</td>
+                    <td>{incomeBudget.paychecks}</td>
+                    <td>{incomeBudget.investments}</td>
+                    <td>{incomeBudget.reimbursements}</td>
+                    <td>{incomeBudget.misc}</td>
                     <td>
                         <Button color="warning" onClick={() => {props.editUpdateIncomeBudget(incomeBudget); props.updateOn()}}>Update Budget</Button>
                         <Button color="danger" onClick={() => {deleteIncome(incomeBudget)}}>Delete Budget</Button>
@@ -38,6 +38,7 @@ const IncomeTable = (props) => {
         <Table>
             <thead>
                 <tr>
+                    <th>#</th>
                     <th>Paychecks</th>
                     <th>Investments</th>
                     <th>Reimbursements</th>
