7c1c88d864112c5a86d5635cd4fc020b5de43927
Quinton Pedrick <77907943+qpedrick@users.noreply.github.com>
2021-12-13 20:46:27 -0500

    added accordion

---
+++ src/components/Budget/BudgetAccordian.jsx
@@ -0,0 +1,60 @@
+import Accordion from 'react-bootstrap/Accordion'
+import { Button } from 'reactstrap'
+import React from "react";
+
+const BudgetAccordian = (props) => {
+    const textStyles = {
+        'textAlign': 'center',
+    }
+
+    const buttonStyles = {
+        width: '200px'
+    }
+
+    return(
+    <div style = {textStyles}>
+    <Accordion>
+        <Accordion.Item eventKey = '0'>
+            <Accordion.Header style = {textStyles}>
+                TotalBudget
+            </Accordion.Header>
+                <Accordion.Body style = {textStyles}>
+                    Insert Table Here
+                </Accordion.Body>
+        </Accordion.Item>
+    </Accordion>
+
+    <br />
+
+    <Accordion>
+        <Accordion.Item eventKey = '0'>
+            <Accordion.Header style = {textStyles}>
+                Income
+            </Accordion.Header>
+                <Accordion.Body style = {textStyles}>
+                    Insert Table Here
+                </Accordion.Body>
+        </Accordion.Item>
+    </Accordion>
+
+    <br />
+
+    <Accordion>
+        <Accordion.Item eventKey = '0'>
+            <Accordion.Header style = {textStyles}>
+                Expenses
+            </Accordion.Header>
+                <Accordion.Body style = {textStyles}>
+                    Insert Table Here
+                </Accordion.Body>
+        </Accordion.Item>
+    </Accordion>
+
+    <br />
+
+    <Button style = {buttonStyles} onClick = {props.clearToken}>Logout</Button>
+
+    </div>
+    )}
+
+export default BudgetAccordian
