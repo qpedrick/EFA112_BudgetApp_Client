0e0ce96822ebf1a6dd493f406b1d9b5c4ce6a97f
Quinton Pedrick <77907943+qpedrick@users.noreply.github.com>
2021-12-15 13:38:32 -0500

    added full budget formatting

--- src/components/Budget/BudgetTable.jsx
+++ src/components/Budget/BudgetTable.jsx
@@ -1,18 +1,16 @@
 import React, {useState, useEffect} from 'react';
-import {Table, Button, Form, FormGroup, Label, Input, Container, Row, Col} from 'reactstrap';
-import IncomeTable from '../Income/IncomeTable';
+import {Table, Button, Container, Row, Col} from 'reactstrap';
+
 
 const BudgetTable = (props) => {
-    const [actualValues, setActualValues] = useState([]);
-
-    const formStyle = {
-        "min-width": "100%" ,
-        "max-width": "100%"
-    }
-
-    // const tbodyStyle = {
-    //     "margin-left": "50rem",
-    // }
+    const [actualPaycheckTotal, setActualPaycheckTotal] = useState(0);
+    const [actualInvestmentTotal, setActualInvestmentTotal] = useState(0);
+    const [actualReimbursementTotal, setActualReimbursementTotal] = useState(0);
+    const [actualMiscTotal, setActualMiscTotal] = useState(0);
+    const [budgetPaycheck, setBudgetPaycheck] = useState(0);
+    const [budgetInvestment, setBudgetInvestment] = useState(0);
+    const [budgetReimbursement, setBudgetReimbursement] = useState(0);
+    const [budgetMisc, setBudgetMisc] = useState(0);
 
     const getIncome = () => {
         fetch('http://localhost:3001/income/', {
@@ -22,102 +20,109 @@ const BudgetTable = (props) => {
                 'Authorization': `Bearer ${props.token}`
             })
         }).then((res) => res.json())
-        .then(res => setActualValues(res[0]))
-        .then(console.log(actualValues))
+        // .then(res => setActualValues(res))
+        .then(res => addIncome(res))
         .catch(err => console.log(err))
     }
+
+    const addIncome = (res) => {
+        if (res.length === 0) {
+            setActualPaycheckTotal(0);
+            setActualInvestmentTotal(0);
+            setActualReimbursementTotal(0);
+            setActualMiscTotal(0);
+        } else {
+        let pTotal = 0;
+        let iTotal = 0;
+        let rTotal = 0;
+        let mTotal = 0;
+        for (let object of res) {
+            pTotal += object.Paychecks;
+            setActualPaycheckTotal(pTotal)
+            iTotal += object.Investments;
+            setActualInvestmentTotal(iTotal)
+            rTotal += object.Reimbursements;
+            setActualReimbursementTotal(rTotal)
+            mTotal += object.Misc;
+            setActualMiscTotal(mTotal)
+        }
+    }
+}
 
 
     return(
         <Container>
-        <Row>
-        <h3>View Full Budget</h3>
-        <br />
-        <br />
-        <br />
-        <hr />
-        <Button onClick = {getIncome}>Get Actual Income values</Button>        
-
-
-        <Col m="3">        
-            <Table>
-            <thead>
-                <tr>
-                    <th>Actual Income</th>
-                    <th>Budget Income</th>
-                    <th>Budget-to-Actuals</th>
-                </tr>
-                <tbody>
-                Paychecks:
-                
-                <br/>
-                <br/>
-                Investments:
-                <br/>
-                <br/>
-                Reimbursements:
-                <br/>
-                <br/>
-                Misc:
-                <br/>
-                <br/>
-            </tbody>
-                <td>        
-                
-        </td>
-            </thead>
-            </Table>
-        </Col>
-            <tbody>
-                
-            </tbody>
-        <br />
-        <br />
-        <br />
-
-        <Table>
-            <thead>
-                <tr>
-                    <th>Actual Expenses</th>
-                    <th>Budget Expenses</th>
-                    <th>Budget-to-Actuals</th>
-                </tr>
-            </thead>
-            <tbody>
-                Transportation:
-                <br/>
-                <br/>
-                Housing:
-                <br/>
-                <br/>
-                Food:
-                <br/>
-                <br/>
-                Personal Care:
-                <br/>
-                <br/>
-                Lifestyle:
-                <br/>
-                <br/>
-                Health:
-                <br/>
-                <br/>
-                Insurance:
-                <br/>
-                <br/>
-                Debt:
-                <br/>
-                <br/>
-                Savings:
-                <br/>
-                <br/>
-                Giving:
-                <br/>
-                <br/>
-            </tbody>
-        </Table>
-        
-        </Row>
+            <Row>
+                <h3>View Full Budget</h3>
+                <Button onClick = {getIncome}>Get Actual Income values</Button>
+            </Row> 
+            <Row>      
+                <Col m="3">
+                    <h5>Actual Income Values</h5>        
+                        <p>
+                        Paychecks:
+                        </p>
+                        <p>
+                        {actualPaycheckTotal}
+                        </p>
+                        <p>
+                        Investments:
+                        </p>
+                        <p>
+                        {actualInvestmentTotal}
+                        </p>
+                        <p>
+                        Reimbursements:
+                        </p>
+                        <p>
+                        {actualReimbursementTotal}
+                        </p>
+                        <p>
+                        Misc:
+                        </p>
+                        <p>
+                        {actualMiscTotal}
+                        </p>
+                </Col>
+                <Col m="3">
+                    <h5>Budget Income Values</h5>        
+                        <p>
+                        Paychecks:
+                        </p>
+                        <input type = 'text' value = {budgetPaycheck} onChange = {(e) => setBudgetPaycheck(e.target.value)}></input>
+                        <p>
+                        Investments:
+                        </p>
+                        <input type = 'text' value = {budgetInvestment} onChange = {(e) => setBudgetInvestment(e.target.value)}></input>
+                        <p>
+                        Reimbursements:
+                        </p>
+                        <input type = 'text' value = {budgetReimbursement} onChange = {(e) => setBudgetReimbursement(e.target.value)}></input>
+                        <p>
+                        Misc:
+                        </p>
+                        <input type = 'text' value = {budgetMisc} onChange = {(e) => setBudgetMisc(e.target.value)}></input>
+                </Col>
+                <Col m="3">
+                    <h5>Difference between Budget and Actual</h5>        
+                        <p>
+                        Paychecks:
+                        </p>
+                        {budgetPaycheck - actualPaycheckTotal}
+                        <p>
+                        Investments:
+                        </p>
+                        {budgetInvestment - actualInvestmentTotal}
+                        <p>
+                        Reimbursements:
+                        </p>
+                        {budgetReimbursement - actualReimbursementTotal}
+                        <p>
+                        Misc:
+                        </p>
+                        {budgetMisc - actualMiscTotal}
+                </Col>
+            </Row>
         </Container>
     )
 }
